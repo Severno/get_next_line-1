01326d703288f5b84989207644153f1c15bcba11
davhojt <davhojt@gmail.com>
2018-03-27 18:00:03 +0300

    Added ft_putstr numbered lines to evaluate behaviour of functions

--- get_next_line.c
+++ get_next_line.c
@@ -6,28 +6,43 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/07 07:49:36 by dhojt             #+#    #+#             */
-/*   Updated: 2018/03/25 15:42:03 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/03/27 17:54:02 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
+static void		refil_buf(char *buf, char *content)
+{
+	if (buf || content)
+		;
+	ft_putstr(buf);
+}
+
 static t_list	*get_fd_live(int fd, t_list **fd_history)
 {
 	t_list	*tmp;
+	ft_putstr("3\n");
 
 	tmp = *fd_history;
+	ft_putstr("4\n");
 
-	while (fd_history)
+	while (tmp)
 	{
+		ft_putstr("5\n");
 		if ((int)tmp->content_size == fd)
 			return (tmp);
 		tmp = tmp->next;
+		ft_putstr("6\n");
 	}
+		ft_putstr("7\n");
 	tmp = ft_lstnew(NULL, fd);
+		ft_putstr("8\n");
 	tmp->next = *fd_history;
+		ft_putstr("9\n");
 	*fd_history = tmp;
+		ft_putstr("10\n");
 	return(tmp);
 }
 
@@ -39,13 +54,17 @@ int				get_next_line(const int fd, char **line)
 	static t_list	*fd_history;
 	t_list			*fd_live;
 
+	ft_putstr("1\n");
 	if (fd < 0 || line == NULL)
 		return (-1);
+	ft_putstr("2\n");
 	fd_live = get_fd_live(fd, &fd_history);
+	ft_putstr("5\n");
+	refil_buf(buf, fd_live->content);
 
 	str = ft_strnew(150);
 	read_result = BUFF_SIZE + 1;
-	
+
 	ft_putstr("LOOP START\n");
 	while(read_result >= BUFF_SIZE)
 	{
@@ -55,7 +74,6 @@ int				get_next_line(const int fd, char **line)
 		//else
 		//	str = ft_strjoin(str, buf);
 		//printf("Read result is %d\n", read_result);
-
 	}
 	ft_putstr("LOOP END\n");
 
