582b5b7b4170e68af68e09937c0eb6258af5bea4
Dav HOJT <davhojt@gmail.com>
2018-04-08 19:58:01 +0200

    Added two fds

--- main.c
+++ main.c
@@ -3,15 +3,24 @@
 
 int		main(void)
 {
-	int		fd;
-	char	*line;
+	int		fd1;
+	int		fd2;
+	char	*line1;
+	char	*line2;
 
-	fd = open("alphabet", O_RDONLY);
-	while ((get_next_line(fd, &line)) == 1)
+	fd1 = open("alphabet", O_RDONLY);
+	fd2 = open("ann", O_RDONLY);
+	while ((get_next_line(fd1, &line1)) == 1)
 	{
-		ft_putstr(line);
+		while ((get_next_line(fd2, &line2)) == 1)
+		{
+			ft_putstr(line2);
+			ft_putchar('\n');
+		}
+		ft_putstr(line1);
 		ft_putchar('\n');
 	}
-	close(fd);
+	close(fd1);
+	close(fd2);
 	return (0);
 }
