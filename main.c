27797d76978d15fac61e6fa678f431cbb075a5bb
davhojt <davhojt@gmail.com>
2018-04-09 02:51:19 +0200

    Made obvious, the return values of line

--- main.c
+++ main.c
@@ -4,23 +4,43 @@
 int		main(void)
 {
 	int		fd1;
-	int		fd2;
+	//int		fd2;
 	char	*line1;
-	char	*line2;
+	//char	*line2;
 
 	fd1 = open("alphabet", O_RDONLY);
-	fd2 = open("ann", O_RDONLY);
+	//fd2 = open("ann", O_RDONLY);
 	while ((get_next_line(fd1, &line1)) == 1)
 	{
-		while ((get_next_line(fd2, &line2)) == 1)
+		/*while ((get_next_line(fd2, &line2)) == 1)
 		{
 			ft_putstr(line2);
 			ft_putchar('\n');
-		}
+		}*/
+		ft_putchar('\n');
+		ft_putchar('\n');
+		ft_putchar('\n');
+		ft_putchar('\n');
+		ft_putchar('\n');
+		ft_putchar('\n');
+		ft_putchar('\n');
+		ft_putchar('\n');
+		ft_putchar('\n');
+		ft_putchar('\n');
 		ft_putstr(line1);
+		ft_putchar('\n');
+		ft_putchar('\n');
+		ft_putchar('\n');
+		ft_putchar('\n');
+		ft_putchar('\n');
+		ft_putchar('\n');
+		ft_putchar('\n');
+		ft_putchar('\n');
+		ft_putchar('\n');
+		ft_putchar('\n');
 		ft_putchar('\n');
 	}
 	close(fd1);
-	close(fd2);
+	//close(fd2);
 	return (0);
 }
