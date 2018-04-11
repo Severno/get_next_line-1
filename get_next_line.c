56861c80fbee7e7178fa28c84569037599854540
davhojt <davhojt@gmail.com>
2018-04-11 09:09:25 +0200

    Reordered content. Added some things to test 42FileChecker

--- get_next_line.c
+++ get_next_line.c
@@ -6,33 +6,53 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/07 07:49:36 by dhojt             #+#    #+#             */
-/*   Updated: 2018/04/10 02:22:41 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/04/11 09:07:44 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
-static char		*join_free(char *content, char *buf)
+/*char	*ft_strjoinch(char const *s1, char c)
 {
-	char	*tmp;
-	
-	tmp = content;
-	content = ft_strjoin(content, buf);
-	free(tmp);
-	return (content);
-}
+	char	*new_str;
+	size_t	i;
+	size_t	s1_len;
 
-static void		split_new_line(char **content, char **str)
+	if (!s1 || !c)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	new_str = ft_strnew(s1_len + 1);
+	if (!new_str)
+		return (NULL);
+	i = -1;
+	while (++i < s1_len)
+		*(new_str + i) = *(s1 + i);
+	*(new_str + i) = c;
+	return (new_str);
+}*/
+
+/*int			ft_strcopychr(char **dst, char *src, char c)
 {
-	if (content)
+	int		i;
+	int		count;
+	int		pos;
+
+	i = -1;
+	count = 0;
+	while (src[++i])
+		if (src[i] == c)
+			break ;
+	pos = i;
+	if (!(*dst = ft_strnew(i)))
+		return (0);
+	while (src[count] && count < i)
 	{
-		*str = ft_strdup(*content);
-		*(ft_strchr(*str, ENDL)) = '\0';
-			
-		*content = ft_strdup(ft_strchr(*content, ENDL) + 1);
+		if (!(*dst = ft_strjoinch(*dst, src[count])))
+			return (0);
+		count++;
 	}
-}
+	return (pos);
+}*/
 
 static t_list	*get_fd_live(int fd, t_list **fd_history)
 {
@@ -57,7 +77,7 @@ static t_list	*get_fd_live(int fd, t_list **fd_history)
 	return(tmp);
 }
 
-int				get_next_line(const int fd, char **line)
+int					get_next_line(const int fd, char **line)
 {
 	char			buf[BUFF_SIZE + 1];
 	char			*str;
@@ -65,38 +85,32 @@ int				get_next_line(const int fd, char **line)
 	static t_list	*fd_history;
 	t_list			*fd_live;
 	int				ret;
+	int				i;
 
 	ret = 1;
-
 	str = NULL;
 	read_result = 0;
 
 	if ((fd < 0 || line == NULL) || (!(fd_live = get_fd_live(fd, &fd_history))))
 		return (-1);
-	
-	//if (!(ft_strchr(fd_live->content, ENDL)))
+	if (read(fd, buf, 0) < 0)
+		return (-1);
+	if(!(*line = ft_strnew(1)))
+		return (-1);
+	while ((read_result = read(fd, buf, BUFF_SIZE)))
 	{
-
-
-		while ((read_result = read(fd, buf, BUFF_SIZE)))//Check while val is 0
-		{
-			if(!(fd_live->content = join_free(fd_live->content, buf)))
-				return (NULL);
-			if (ft_strchr(buf, '\n'))
-			{
-				break ;
-			}
-
-		}
+		buf[read_result] = '\0';
+		if (!(fd_live->content = ft_strjoin(fd_live->content, buf)))
+			return (-1);;
+		if (ft_strchr(buf, '\n'))
+			break ;
 	}
-
-	if (!read_result && !ft_strchr(fd_live->content, ENDL))
-	{
-		*line = ft_strdup(fd_live->content);
+	if (read_result < BUFF_SIZE && !ft_strlen(fd_live->content))
 		return (0);
-	}
+	i = ft_strcopychr(line, fd_live->content, '\n');
+	if (i < (int)ft_strlen(fd_live->content))
+		fd_live->content += (i + 1);
 	else
-		split_new_line((char **)&fd_live->content, &str);
-	*line = str;
+		ft_strclr(fd_live->content);
 	return (1);
 }
