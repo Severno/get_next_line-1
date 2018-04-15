4828b3eb53944c8af9d63f1db6ceadae1ec49a46
Dav HOJT <davhojt@gmail.com>
2018-04-15 20:03:59 +0200

    Main now uses ft_putstr

--- main.c
+++ main.c
@@ -7,28 +7,15 @@ int		main(int argc, char **argv)
 	if (argc == 2)
 	{
 	int		fd1;
-	//int		fd2;
 	char	*line1;
-	//char	*line2;
 	int i;
 
 	i = 0;
 
 	fd1 = open(argv[1], O_RDONLY);
-	//fd2 = open("ann", O_RDONLY);
 	while ((get_next_line(fd1, &line1)) == 1)
-	{
-		/*while ((get_next_line(fd2, &line2)) == 1)
-		{
-			ft_putstr(line2);
-			ft_putchar('\n');
-			ft_putchar('\n');
-		}*/
-		printf("LINE %d: [%s] END\n", i++, line1);
-		free(line1);
-	}
+		ft_putstr(line1);
 	close(fd1);
-	//close(fd2);
 	}
 	return (0);
 }
